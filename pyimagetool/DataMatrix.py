Bug fixes and unit tests - Created a few unit tests to test slicing and binning - Fixed an off by one error in binning window - Enhanced RegularDataArray by adding dims, which are displayed by PyImageTool - Fixed a bug where a numpy array could not be loaded into PyImageTool.

pyimagetool/DataMatrix.py
@@ -10,12 +10,13 @@ class RegularDataArray(object):
     defines properties relevant to the regular grid
     """
 
-    def __init__(self, dat=None, axes=None, delta=None, coord_min=None):
+    def __init__(self, dat=None, axes=None, delta=None, coord_min=None, dims=None):
         """Create an instance of a RegularDataArray
         :param dat: RegularDataArray, xarray.DataArray, or numpy.array
         :param axes: Iterable of numpy arrays representing coordinates
         :param delta: Iterable representing the delta for each axis
         :param coord_min: Iterable representing the offset/coord_min for each axis
+        :param dims: Iterable representing names of each dimension
         """
         if dat is None:
             return
@@ -60,13 +61,20 @@ class RegularDataArray(object):
         self.axes = [self.coord_min[i] + self.delta[i]*np.arange(dat.shape[i]) for i in range(dat.ndim)]
         self.coord_max = np.array([ax[-1] for ax in self.axes])
         coords = {}
-        label = ['x', 'y', 'z', 't']
-        for la, i in zip(label, range(dat.ndim)):
-            coords[la] = self.axes[i]
-        if dat.ndim > len(label):
-            for i in range(len(label), dat.ndim):
-                coords['dim_' + str(i)] = self.axes[i]
-        self._data: xr.DataArray = xr.DataArray(dat, dims=coords.keys(), coords=coords)
+        if dims is None:
+            label = ['x', 'y', 'z', 't']
+            for la, i in zip(label, range(dat.ndim)):
+                coords[la] = self.axes[i]
+            if dat.ndim > len(label):
+                for i in range(len(label), dat.ndim):
+                    coords['dim_' + str(i)] = self.axes[i]
+            dims = coords.keys()
+        else:
+            if len(dims) != len(self.axes):
+                raise ValueError('Not all dimensions are labelled')
+            for i, dim in enumerate(dims):
+                coords[dim] = self.axes[i]
+        self._data: xr.DataArray = xr.DataArray(dat, dims=dims, coords=coords)
 
     def data_updated(self, regular=True):
         if regular:
@@ -140,26 +148,26 @@ class RegularDataArray(object):
         return self.coord_min
 
     @staticmethod
-    def from_numpy_array(dat: np.array, axes=None, delta=None, coord_min=None):
+    def from_numpy_array(dat: np.array, axes=None, delta=None, coord_min=None, dims=None):
         """
         Build data using a numpy array. Must provide one of the following:
         - An iterable of axes, where each axis is a numpy array
         - An interable of delta and coord_min
         Guarantees regularly gridded coordinates and sorts data so that coordinates are ascending
         """
-        return RegularDataArray(dat, axes=axes, delta=delta, coord_min=coord_min)
+        return RegularDataArray(dat, axes=axes, delta=delta, coord_min=coord_min, dims=dims)
 
     @staticmethod
     def from_xarray(dat: xr.DataArray):
         coord_min = np.array([dat[dim].values[0] for dim in dat.dims])
         delta = np.array([dat[dim].values[1] - dat[dim].values[0] for dim in dat.dims])
-        return RegularDataArray(dat.values, delta=delta, coord_min=coord_min)
+        return RegularDataArray(dat.values, delta=delta, coord_min=coord_min, dims=dat.dims)
 
     @staticmethod
     def from_xarray_irregular(dat: xr.DataArray):
         coord_min = np.array([dat[dim].values[0] for dim in dat.dims])
         delta = np.array([np.mean(np.diff(dat[dim].values)) for dim in dat.dims])
-        return RegularDataArray(dat.values, delta=delta, coord_min=coord_min)
+        return RegularDataArray(dat.values, delta=delta, coord_min=coord_min, dims=dat.dims)
 
 
 class RegularSpacedData(object):

pyimagetool/ImageTool.py
@@ -24,9 +24,13 @@ class ImageTool(QtWidgets.QWidget):
         """
         super().__init__(parent)
         # Warn user about nan
-        if np.any(np.isnan(data.values)):
+        if hasattr(data, 'values'):
+            d = data.values
+        else:
+            d = data
+        if np.any(np.isnan(d)):
             warnings.warn('Input data contains NaNs. All NaN will be set to 0.')
-            data.values[np.isnan(data.values)] = 0
+            d[np.isnan(d)] = 0
         # Create data
         self.data: RegularDataArray = RegularDataArray(data)
         self.it_layout: int = layout

pyimagetool/PGImageTool.py
@@ -409,7 +409,8 @@ class Cursor:
         self._index: List[SingleValueModel] = [SingleValueModel(0) for i in range(data.ndim)]
         self._pos: List[SingleValueModel] = [SingleValueModel(float(self.data.coord_min[i])) for i in range(data.ndim)]
         self._binwidth: List[SingleValueModel] = [SingleValueModel(data.delta[i]) for i in range(data.ndim)]
-        self._binpos: List[List[float]] = [[0, 0] for i in range(data.ndim)]
+        self._binpos: List[List[float]] = [[data.coord_min[i], data.coord_min[i] + data.delta[i]/2]
+                                           for i in range(data.ndim)]
 
     @property
     def pos(self):
@@ -438,12 +439,15 @@ class Cursor:
         return self._index[i].val
 
     def get_index_slice(self, i):
+        """Using the known binwidth and bin positions, calculate a slice in index space
+        Note: if the binwidth <= delta (or the bin index is 1), there will never be any binning
+        """
         if self._binwidth[i].val <= self.data.delta[i]:
             return slice(self._index[i].val, self._index[i].val + 1)
         else:
             mn = int(np.ceil(self.data.scale_to_index(i, self._binpos[i][0])))
             mx = int(np.floor(self.data.scale_to_index(i, self._binpos[i][1])))
-            return slice(mn, mx)
+            return slice(mn, mx + 1)
 
     def get_binwidth(self, i):
         return self._binwidth[i].val